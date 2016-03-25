9a937eb53f3aa3dc08bd39262fafb3f5e7655922
dborncamp <dborncamp@stsci.edu>
2016-03-25 18:10:22 -0400

    Added Assignment2/problems to send to professor because towson wont let me send a tarball

---
+++ Assignment2_686/problems/2objects.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var canvas;
+var gl;
+var myCube;
+var program;
+
+var NumVertices = 36;
+
+
+window.onload = function init(){
+//    try {
+        // get the canvas
+        canvas = document.getElementById( "gl-canvas" );
+
+        // setup the canvas
+        gl = WebGLUtils.setupWebGL( canvas );
+        if ( !gl ) { alert( "WebGL isn't available" ); }
+
+        gl.viewport( 0, 0, canvas.width, canvas.height );
+        gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+        gl.enable(gl.DEPTH_TEST);
+
+        // make the cube
+        myCube = cube(1);
+
+        // get the canvas and set up the program
+        program = initShaders( gl, "vertex-shader", "fragment-shader" );
+        gl.useProgram( program );
+
+        // create a buffer to start loading the data onto the GPU
+        console.log(myCube.TriangleVertices);  // debugging line
+        var cBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(myCube.VertexColors), gl.STATIC_DRAW );
+
+        var vColor = gl.getAttribLocation( program, "vColor" );
+        gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vColor );
+
+        var vBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(myCube.TriangleVertices), gl.STATIC_DRAW );
+
+        var vPosition = gl.getAttribLocation( program, "vPosition" );
+        gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vPosition );
+
+        // Draw it
+        gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+        //render()
+//    } catch (e) {
+//      alert("Could not initialize WebGL! " + e);
+//      return;
+//   }
+}
+
+function render(){
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+}
